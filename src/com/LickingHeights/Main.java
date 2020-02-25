3759e909f7b8cac791a8476864217a32313894e1
Jasmine <jasmine.pilkerton.2022@student.lhschools.org>
2020-02-25 12:09:50 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,119 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // Phase 1
+       String typeOfBird;
+
+       String adjective;
+
+       String verbPastTense;
+
+       String noun;
+
+       String adjective2;
+
+       String verb;
+
+       String relativesName;
+
+       String verbEndingInIng;
+
+       String partOfBodyPlural;
+
+       String liquid;
+
+       String pluralNoun;
+
+       String verbEndingInIng2;
+
+       String noun2;
+
+       String roomInAHouse;
+
+       String verb2;
+
+       String room;
+
+       String verb3;
+
+       Scanner keyboard;
+
+
+       keyboard = new Scanner(System.in);
+
+       System.out.println("adjective");
+       adjective = keyboard.nextLine();
+
+       System.out.println("adjective2");
+       adjective2 = keyboard.nextLine();
+
+       System.out.println("Type of bird");
+       typeOfBird = keyboard.nextLine();
+
+       System.out.println("room in a house");
+       roomInAHouse= keyboard.nextLine();
+
+       System.out.println("Verb past tense");
+       verbPastTense = keyboard.nextLine();
+
+       System.out.println("verb");
+       verb = keyboard.nextLine();
+
+       System.out.println("Relatives name");
+       relativesName = keyboard.nextLine();
+
+       System.out.println("noun");
+       noun = keyboard.nextLine();
+
+       System.out.println("liquid");
+       liquid = keyboard.nextLine();
+
+       System.out.println("Verb ending in ing");
+       verbEndingInIng = keyboard.nextLine();
+
+       System.out.println("Part of body plural");
+       partOfBodyPlural = keyboard.nextLine();
+
+       System.out.println("pluralNoun");
+       pluralNoun = keyboard.nextLine();
+
+       System.out.println("verb ending in ing 2");
+       verbEndingInIng2 = keyboard.nextLine();
+
+       System.out.println("noun2");
+       noun2 = keyboard.nextLine();
+
+       System.out.println("verb2");
+      verb2 = keyboard.nextLine();
+
+       System.out.println("room");
+       room = keyboard.nextLine();
+
+       System.out.println("verb3");
+       verb3 = keyboard.nextLine();
+
+       //Phase 3
+       System.out.println("It was a " + adjective + " cold november day.");
+       System.out.println("I woke up to the " + adjective2 + " smell of " + typeOfBird + " roasting in the " + roomInAHouse + " downstairs." );
+       System.out.println("I " + verbPastTense + " down the stairs to see if i could help " + verb + " the dinner ");
+       System.out.println("My mom said, see if " + relativesName + " needs a fresh " + noun );
+       System.out.println("So I carried a tray of glasses full of " + liquid + " into the " + verbEndingInIng + " room");
+       System.out.println("When i got there, I couldn't believe my " + partOfBodyPlural + "!");
+       System.out.println("There were " + pluralNoun + verbEndingInIng2 + " on the " + noun2 + "!");
+       System.out.println("I " + verb2 + " out of the room in shock.");
+       System.out.println("I ran to the " + room + " snd " + verb3 + " onto the floor.");
+
+
+
+
+
+
+
+
+
+    }
+}
